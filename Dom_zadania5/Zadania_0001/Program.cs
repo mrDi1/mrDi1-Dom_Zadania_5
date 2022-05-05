c2de752470fc2fa61de469c7c1c708750fb4614f
Daulet <moni_i@mail.ru>
2022-05-05 19:04:40 +0600

    Задача 1

---
+++ Dom_zadania5/Zadania_0001/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 1
+// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве
+
+
+// - создать массив
+// - ввод данных, рандом, положительные, трёхзначные
+// - опридиления четных чисел в массиве
+
+
+// - создать массив
+int[] Array(int count)
+{
+    int[] res = new int[count];
+    return res;
+}
+
+// - ввод данных рандом
+void RandomArray(int[] array, int min, int max)
+{
+    int len = array.Length;
+    for (int i = 0; i < len; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+}
+
+// - вывод на консоль
+string ConvertArray(int[] array)
+{
+    int len = array.Length;
+    string result = string.Empty;
+    for (int i = 0; i < len; i++)
+    {
+        result = result + $"{array[i]} ";
+    }
+    return result;
+}
+
+// // - опридиления четных чисел в массиве
+// int[] ArrayChet(int[] array)
+// {
+//     // int res = 0;
+//     int len = array.Length;
+//     int[] result = new int[len];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] % 2 == 0) result[i]++;
+//     }
+//     return result;
+// }
+
+
+// - Основной код
+
+int[] inArr = Array(10);                           // - создаем массив состоящи из 10 элиментов
+RandomArray(inArr, 100, 999);                     // - ввод данных в рандом
+string outStr = ConvertArray(inArr);              // - конвертируем строку в int (дано)
+Console.WriteLine(outStr);                        // - выводим массива (дано)
+
+int res = 0;
+for (int i = 0; i < inArr.Length; i++)
+{
+    if (inArr[i] % 2 == 0) res++;
+}
+Console.WriteLine("Чётных чисел в массиве: "+res);
+
+
+
+
+
+
+
+
